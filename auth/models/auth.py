e3e7398d8a33347d5308eda339ed16076a2eadd3
rvale01 <rvale09@gmail.com>
2023-02-24 16:14:08 +0000

    added comments to the models

--- auth/models/auth.py
+++ auth/models/auth.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Table which includes all the users, the type is also specified for each user.
+# Based on the type, the other appropriate table should be checked
 class AllUsers(models.Model):
     USER_TYPES = (
         ('student', 'student'),
         ('cinema_manager', 'Cinema Manager'),
         ('account_manager', 'Account Manager'),
-        ('cinema_manager', 'Cinema Manager'),
+        ('club', 'Club'),
     )
     type = models.CharField(max_length=20, choices=USER_TYPES)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+
+# This table includes all the students account and their details
 class StudentAccounts(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     balance = models.IntegerField()
@@ -20,6 +24,7 @@ class StudentAccounts(models.Model):
     email = models.CharField(max_length=100)
     discount = models.IntegerField()
 
+# This table includes all the clubs accounts and their details
 class Clubs(models.Model):
     balance = models.IntegerField()
     club = models.ForeignKey(User, on_delete=models.CASCADE)
