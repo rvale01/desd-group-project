bcbd39455c5fadcb973e1e4cdc54b83c0b6e658a
Hamad Almahmoud <hamadalmahmoud@192.168.1.135>
2023-03-07 17:59:18 +0000

    created login functionality and modified URL to support login

--- customAuth/urls.py
+++ customAuth/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from django.urls import path, include
 from .views.registration import registrationCustomer
+from .views.login import loginCustomer
+
 
 urlpatterns = []
 
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
     path('registration/customer', registrationCustomer, name = 'registrationCustomer'),
+    path('login/', loginCustomer, name= 'loginCustomer'),
     # path('accounts/delete', views.delete_account),
 ]
--- customAuth/views/login.py
+++ customAuth/views/login.py
@@ -1,3 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
-
-# Write function here
+def loginCustomer(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("/")
+            else:
+                messages.error(request,"Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="auth/login.html", context={"login_form":form})
