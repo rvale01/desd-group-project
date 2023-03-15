d3cfb8f3b56dd50d7d2f1a01e43c8f178d0d2b7a
rvale01 <rvale09@gmail.com>
2023-03-15 17:47:52 +0000

    added cinema manager folder

---
+++ cinemaManager/views/film.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from ..forms import FilmForm
+from ...group_14.models.general import Film 
+
+def addFilmForm(request):
+    if request.method == "GET":
+        return render(request, 'AddFilm.html')
+
+def addFilm(request):
+    if request.method == "POST":
+        form = FilmForm(request.POST)
+        if form.is_valid():
+            # Get form data
+            title = form.cleaned_data.get('title')
+            description = form.cleaned_data.get('description')
+            age_rating = form.cleaned_data.get('age_rating')
+            duration = form.cleaned_data.get('duration')
+            
+            # Create new Film object
+            film = Film(title=title, description=description, age_rating=age_rating, duration=duration)
+            
+            # Save the new Film object to the database
+            film.save()
+            
+            # Redirect to homepage
+            return redirect('home')
+    return redirect('home')
