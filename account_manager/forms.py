366e26e41dff84f1d695711c50b4d63903e2ebdf
rvale01 <rvale09@gmail.com>
2023-03-17 15:07:50 +0000

    Revert "asa"
    This reverts commit 074c7e9064f4e11fcc83d51b98d0b3350d22d0fc.

--- account_manager/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from models import Film
-
-class FilmForm(forms.ModelForm):
-    class Meta:
-        model = Film
-        fields = ['title','release_year', 'genre', 'description']
