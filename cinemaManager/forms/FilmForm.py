816eb90d31bc8ba2372bfa108737478f3e2b879f
rvale01 <rvale09@gmail.com>
2023-03-17 16:19:15 +0000

    commented out films details

--- cinemaManager/forms/FilmForm.py
+++ cinemaManager/forms/FilmForm.py
@@ -1,7 +1,7 @@
-from django import forms
-from group_14.models.general import Film
+# from django import forms
+# from group_14.models.general import Film
 
-class FilmForm(forms.ModelForm):
-    class Meta:
-        model = Film
-        fields = '__all__'
+# class FilmForm(forms.ModelForm):
+#     class Meta:
+#         model = Film
+#         fields = '__all__'
--- cinemaManager/views/film.py
+++ cinemaManager/views/film.py
@@ -1,27 +1,33 @@
-from django.shortcuts import render, redirect
-from ..forms import FilmForm
-from group_14.models.general import Film 
+# from django.shortcuts import render, redirect
+# from ..forms import FilmForm
+# from group_14.models.general import Film 
 
-def addFilmForm(request):
-    if request.method == "GET":
-        return render(request, 'AddFilm.html')
+# def addFilmForm(request):
+#     if request.method == "GET":
+#         return render(request, 'AddFilm.html')
 
-def addFilm(request):
-    if request.method == "POST":
-        form = FilmForm(request.POST)
-        if form.is_valid():
-            # Get form data
-            title = form.cleaned_data.get('title')
-            description = form.cleaned_data.get('description')
-            age_rating = form.cleaned_data.get('age_rating')
-            duration = form.cleaned_data.get('duration')
+# def addFilm(request):
+#     if request.method == "POST":
+#         form = FilmForm(request.POST)
+#         if form.is_valid():
+#             # Get form data
+#             title = form.cleaned_data.get('title')
+#             description = form.cleaned_data.get('description')
+#             age_rating = form.cleaned_data.get('age_rating')
+#             duration = form.cleaned_data.get('duration')
             
-            # Create new Film object
-            film = Film(title=title, description=description, age_rating=age_rating, duration=duration)
+#             # Create new Film object
+#             film = Film(title=title, description=description, age_rating=age_rating, duration=duration)
             
-            # Save the new Film object to the database
-            film.save()
+#             # Save the new Film object to the database
+#             film.save()
             
-            # Redirect to homepage
-            return redirect('home')
-    return redirect('home')
+#             # Redirect to homepage
+#             return redirect('home')
+#     return redirect('home')
+
+# def delete_film(request, film_id):
+#     if request.method == "POST":
+#         film = Film.objects.get(id = film_id)
+#         film.delete()
+#     return redirect('deleted_complete_view')
