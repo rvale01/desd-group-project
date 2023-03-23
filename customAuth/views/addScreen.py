5ad77de9962f0d286589f0e570edc3d96415dc15
Hamad Almahmoud <hamadalmahmoud@Hamads-MacBook-Pro-2.local>
2023-03-23 16:43:11 +0000

    added class meta for addscreen form

--- customAuth/views/addScreen.py
+++ customAuth/views/addScreen.py
@@ -4,6 +4,9 @@ from django import forms
 
 # form class
 class AddScreenForm(forms.Form):
+    class Meta:
+        model = Screen
+        fields = ["screen_id", "no_seats"]
     screen_id = forms.IntegerField()
     no_seats = forms.IntegerField()
     
@@ -11,8 +14,10 @@ def addScreen(request):
     if request.method == 'POST':
         form = AddScreenForm(request.POST)          #form objects
         if form.is_valid(): 
-            save_screen_obj = Screen.objects.create(screen_id = form.cleaned_data['screen_id'], no_seats = form.cleaned_data['no_seats'])
-            save_screen_obj.save()
+            # save_screen_obj = Screen.objects.create(screen_id = form.cleaned_data['screen_id'], no_seats = form.cleaned_data['no_seats'])
+            # save_screen_obj.save()
+            screen = Screen(screen_id=form.cleaned_data['screen_id'], no_seats=form.cleaned_data['no_seats'])
+            screen.save()
             return redirect("success!")
         else:
             form = AddScreenForm()
