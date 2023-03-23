45f25fb94cecf3ed6235d0f954f54b70d4e73a80
Hamad Almahmoud <hamadalmahmoud@Hamads-MacBook-Pro-2.local>
2023-03-23 17:01:38 +0000

    added default=0 to balance field

--- customAuth/models/auth.py
+++ customAuth/models/auth.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # This table includes all the students account and their details
 class StudentAccounts(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    balance = models.IntegerField()
+    balance = models.IntegerField(default=0)
     street_no = models.CharField(max_length=100)
     city =  models.CharField(max_length=100)
     mobile_phone = models.CharField(max_length=100)
@@ -13,7 +13,7 @@ class StudentAccounts(models.Model):
 
 # This table includes all the clubs accounts and their details
 class Clubs(models.Model):
-    balance = models.IntegerField()
+    balance = models.IntegerField(default=0)
     club = models.ForeignKey(User, on_delete=models.CASCADE)
     club_name = models.CharField(max_length=100)
     rep_fname = models.CharField(max_length=100)
