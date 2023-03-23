42d24981e1692f2b36737bfd17a6ab87aec08df4
rvale01 <rvale09@gmail.com>
2023-03-23 23:30:21 +0000

    fixed showing delete function and view

--- cinemaManager/views/showing.py
+++ cinemaManager/views/showing.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from ..forms.ShowingForm import ShowingForm
 from ..models.general import Showing 
+import datetime
 
 def addShowingForm(request):
     form = ShowingForm()
@@ -10,16 +11,40 @@ def addShowingForm(request):
 def addShowing(request):
     if request.method == "POST":
         form = ShowingForm(request.POST)
-        if form.is_valid():            
-            form.save()
-            return redirect('showingList')
-    return redirect('showingList')
+        if form.is_valid():
+            showing = form.save(commit=False)
+            screen = showing.screen
+            available_seats = form.cleaned_data['available_seats']
+            
+            # Check if there are no other showings at the same screen on the same day and time
+            date = showing.date
+            start_time = showing.time
+            end_time = (datetime.combine(date, start_time) + timedelta(minutes=showing.film.duration)).time()
+            conflicting_showings = Showing.objects.filter(
+                screen=screen, 
+                date=date, 
+                time__range=(start_time, end_time)
+            ).exclude(showing_id=showing.showing_id)
+            
+            if conflicting_showings.exists():
+                form.add_error(None, 'There is already a showing at this screen at the same date and time.')
+                return render(request, 'Showings/AddShowing.html', {'form':form})
+            
+            if(screen.no_seats >= available_seats):
+                showing.save()
+            else:
+                form.add_error('available_seats', 'Not enough seats available.')
+                return render(request, 'Showings/AddShowing.html', {'form':form})
+    else:
+        form = ShowingForm()
+    return render(request, 'Showings/AddShowing.html', {'form':form})
+
 
 def deleteShowing(request):
     if request.method == 'POST':
         showing_id = request.POST.get('showing_id')
         if showing_id:
-            Showing.objects.filter(id=showing_id).delete()
+            Showing.objects.filter(showing_id=showing_id).delete()
             return redirect('showingList')
     showings = Showing.objects.all()
     context = {'showings': showings}
@@ -32,7 +57,7 @@ def showingList(request):
 
 
 def editShowing(request, showing_id):
-    showing = Showing.objects.get(id=showing_id)
+    showing = Showing.objects.get(showing_id=showing_id)
 
     if request.method == 'POST':
         form = ShowingForm(request.POST, instance=showing)
