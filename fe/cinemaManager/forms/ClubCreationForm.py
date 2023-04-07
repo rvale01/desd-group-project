9ad3ff7da3a4cea54da2a980c8a7cb21dbfa2a22
rvale01 <rvale09@gmail.com>
2023-04-07 20:46:07 +0100

    fixed club manager form

--- fe/cinemaManager/forms/ClubCreationForm.py
+++ fe/cinemaManager/forms/ClubCreationForm.py
@@ -5,4 +5,4 @@ from django import forms
 class ClubCreationForm(forms.ModelForm):
     class Meta:
         model = Clubs
-        fields = ['club_name', 'rep_fname', 'rep_lname', 'street_no', 'city', 'mobile_phone', 'email']
+        fields = "__all__"
