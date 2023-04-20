fd5c81cb514801fba139e76b3cfe250414d577fe
a2-bayomi <ahmed2.bayomi@live.uwe.ac.uk>
2023-04-20 18:18:34 +0000

    Update tests.py

--- fe/cinemaManager/tests.py
+++ fe/cinemaManager/tests.py
@@ -1,7 +1,1 @@
 from django.test import TestCase
-from django.urls import reverse
-
-class TestMyView(TestCase):
-    def test_my_view(self):
-        response = self.client.get(reverse('my-view'))
-        self.assertEqual(response.status_code, 200)
