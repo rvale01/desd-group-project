6a4382cdd6cf980cc29038fc77f22e62308ece1d
rvale01 <rvale09@gmail.com>
2023-05-01 22:30:48 +0100

    fixes to payment flow

--- fe/customer/views/payment.py
+++ fe/customer/views/payment.py
@@ -2,9 +2,21 @@
 from django.shortcuts import render
 from cinemaManager.models.general import Booking, Showing
 
+ADULTS_TICKET_PRICE = 10
+CHILDREN_TICKET_PRICE = 7
 
+def handle_successful_payment(request):
+    showing_id = request.session.get('showing_id')
+    
+    adults_tickets = request.session.get('adults_tickets')
+    adults_tickets = int(adults_tickets) if adults_tickets is not None else 0
+    
+    children_tickets = request.session.get('children_tickets')
+    children_tickets = int(children_tickets) if children_tickets is not None else 0
 
-def handle_successful_payment(request, showing_id, adults_tickets, children_tickets, total_cost, response):
+    total_cost = (adults_tickets) * (ADULTS_TICKET_PRICE)
+    total_cost += (children_tickets) * (CHILDREN_TICKET_PRICE)
+
     # Update the showing.available_seats
     showing = Showing.objects.get(showing_id=showing_id)
     showing.available_seats -= (adults_tickets + children_tickets)
