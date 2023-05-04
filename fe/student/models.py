de0a0ed571b0f88678a004a023a0d8070deaea80
rvale01 <rvale09@gmail.com>
2023-05-05 00:33:05 +0100

    removed wrong model

--- fe/student/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    credit = models.DecimalField(max_digits=6, decimal_places=2, default=0)
