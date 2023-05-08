edef76d007d0a72fffae10966e9b4a76bacad053
rvale01 <rvale09@gmail.com>
2023-05-08 18:25:32 +0100

    fix service

--- services/payment_system/urls.py
+++ services/payment_system/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import create_default_checkout_session
+from .views import create_default_checkout_session, add_credit
 
 urlpatterns = [
     path('create-tickets-session/', create_default_checkout_session, name='create_default_checkout_session'),
-    path('add-credit/')
+    path('add-credit/', add_credit, name="add_credit")
 ]
